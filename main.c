990a8ceb2f9b58963444718bd99b22e8c18e0a68
Edson Macias <edfmc@hotmail.com>
2021-05-24 07:52:21 -0500

    V 0.1 Trabajo inicial

---
+++ main.c
@@ -0,0 +1,11 @@
+# include <stdio.h> 
+# include "D:\Training\GIT\Ejercicios\Ejercicio2\Libraries\gpio.h"
+
+int main( )
+{
+	printf(" Introducción a la programación en lenguaje C");
+	return 0;
+}
+
+
+
