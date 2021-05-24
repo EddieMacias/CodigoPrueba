bc23e9bfc99f9487b2ca2de5d88cba638de3eff4
Edson Macias <edfmc@hotmail.com>
2021-05-24 10:08:23 -0500

    VD 0.2 mejoras finalizadas

--- Libraries/Functions.h
+++ Libraries/Functions.h
@@ -5,3 +5,9 @@ uint8_t suma(uint8_t a,uint8_t b)
 
 }
 
+
+int resta(uint8_t a,uint8_t b)
+{
+	return a-b;
+
+}
--- main.c
+++ main.c
@@ -4,6 +4,13 @@
 
 int main( )
 {
+	uint8_t a = 2;
+	uint8_t b = 3;
+	uint8_t c = 0;
+	
+	c = suma (a,b)
+	
+	
 	printf(" Introducción a la programación en lenguaje C");
 	return 0;
 }
